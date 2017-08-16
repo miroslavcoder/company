6b02abfbcd129bc104f11b1772c8ad29e24c14fe
miro <miroslavphoto@gmail.com>
2017-08-16 20:41:25 +0300

    add models header

--- main/models.py
+++ main/models.py
@@ -7,11 +7,11 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 
 class Header(models.Model):
-    title = models.CharField(, max_length=250)
-    description = models.CharField(, max_length=250)
-    cover_photo = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
-    created_at = models.DateTimeField(, auto_now=True, auto_now_add=False)
-    updated_at = models.DateTimeField(, auto_now=False, auto_now_add=True)
+    title = models.CharField( max_length=250)
+    description = models.CharField( max_length=250)
+    cover_photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    updated_at = models.DateTimeField( auto_now=False, auto_now_add=True)
 
     def __str__(self):
         return self.title
--- port/settings/base.py
+++ port/settings/base.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+    
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
