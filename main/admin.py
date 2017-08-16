ec0697358ee3e6af1404a176db19b3761bb0e60e
miro <miroslavphoto@gmail.com>
2017-08-16 22:55:25 +0300

    add models Parallax

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Header
+from .models import Header , About , Service , Parallax
 # Register your models here.
 
 
 
-admin.site.register(Header)
+admin.site.register(Header, About , Service )
