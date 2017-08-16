f1d89d592443c2c34b3a664ec5025e3f2081fd7c
miro <miroslavphoto@gmail.com>
2017-08-16 22:31:56 +0300

    add models service

--- main/models.py
+++ main/models.py
@@ -22,13 +22,37 @@ class About(models.Model):
      about_description = models.CharField( max_length=250)
      about_photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
      experience_year  = models.CharField( max_length=50)
-     experience  = models.CharField( max_length=50)
      about_title2 = models.CharField( max_length=250)
      about_description2 = models.CharField( max_length=250)
      about_photo2 = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
-     
+     about_row1 = models.CharField( max_length=250)
+     about_row2 = models.CharField( max_length=250)
+     about_row3 = models.CharField( max_length=250)  
      created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
      updated_at = models.DateTimeField( auto_now=False, auto_now_add=True)
-     
+
      def __str__(self):
-        return self.title
+        return self.about_title
+
+#Service
+class Service(models.Model):
+     service_title = models.CharField( max_length=250)
+     service_description = models.CharField( max_length=250)
+     service_photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+
+     service_title2_small = models.CharField( max_length=250)
+     service_title2 = models.CharField( max_length=250)
+     
+
+     service_description2 = models.CharField( max_length=250)
+     service_photo2 = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+
+     service_row1 = models.CharField( max_length=250)
+     service_row2 = models.CharField( max_length=250)
+     service_row3 = models.CharField( max_length=250)  
+
+     created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+     updated_at = models.DateTimeField( auto_now=False, auto_now_add=True)
+
+     def __str__(self):
+        return self.service_title
